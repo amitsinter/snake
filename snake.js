98f512dd1765303bf19f0ecc6575f007ab68c30c
AmitSinter <50829299+amitsinter@users.noreply.github.com>
2021-04-25 18:26:09 +0300

    Add files via upload

--- snake.js
+++ snake.js
@@ -17,7 +17,7 @@ let snake = [
     {x: 170, y:200},
     {x: 160, y:200}
 ];
-
+let changing_direction = false;
   /* Horizontal velocity */
   let dx = 10;
   /* Vertical velocity */
@@ -29,9 +29,13 @@ const snake_board_x = snake_board.getContext("2d");
 
 startGame();
 
+document.addEventListener("keydown",direction);
 
 function startGame()
 {
+    if(ifGameEnded())
+        return;
+        changing_direction = false;
     setTimeout(function onTick()
     {
         clearBoard();
@@ -65,11 +69,83 @@ function drawSnakeParts(snake_part)
     snake_board_x.strokeRect(snake_part.x,snake_part.y,10,10);
 }
 
+/* preventing the snake from moving infinitely */
+function ifGameEnded()
+{
+    /* checking if the heaf of the snake collied with any of the body parts. */
+    for(let i=4; i<snake.length; i++)
+    {
+        const collided = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
+        if(collided)
+        {
+            return true;
+        }
+    }
+    /* checking the boundry walls */
+        const hit_left_wall = snake[0].x < 0;
+        const hit_top_wall = snake[0].y < 0;
+        const hit_right_wall = snake[0].x > snake_board.width -10;
+        const hit_bottom_wall = snake[0].y > snake_board.height -10;
+        return hit_left_wall || hit_top_wall || hit_right_wall || hit_bottom_wall;
+    
+}
+
+
+
+
+/* when pressing on the arrows to change the directions of the snake */
+function direction(event)
+{
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+    //const PAUSE_KEY = 37;
+    if(changing_direction)
+        return;
+    changing_direction = true;
+    const key_pressed = event.keyCode;
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+
+    if(key_pressed === LEFT_KEY && !goingRight)
+    {
+        dx = -10;
+        dy = 0;
+    }
+    if(key_pressed === UP_KEY && !goingDown)
+    {
+        dx = 0;
+        dy = -10;
+    }
+
+    if(key_pressed === RIGHT_KEY && !goingLeft)
+    {
+        dx = 10;
+        dy = 0;
+    }
+    if(key_pressed === DOWN_KEY && !goingUp)
+    {
+        dx = 0;
+        dy = 10;
+    }
+}
+
+
+
 function moveSnake()
 {
     const head = {x: snake[0].x + dx, y: snake[0].y + dy};
     snake.unshift(head);
     snake.pop();
 }
+
+
+
+
+
+
 
 
